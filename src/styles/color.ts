3cf0d2dd5ea0a4595b4fe7532be84b5d2702c7bf
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-11-30 14:47:38 -0300

    refactor: trocando as cores do app

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,15 +1,74 @@
 const colors = {
-    primary: "#0055A4", // Azul forte, inspirado no fundo principal
-    secondary: "#FF6A00", // Laranja vibrante para contraste e botões
-    background: "#0D1B2A", // Azul-escuro profundo para o fundo geral
-    surface: "#1B263B", // Azul-acinzentado para cartões ou áreas secundárias
-    textPrimary: "#FFFFFF", // Branco para textos principais em fundos escuros
-    textSecondary: "#B0BEC5", // Cinza claro para subtítulos ou descrições
-    accent: "#9C27B0", // Roxo vibrante presente no coral
-    success: "#4CAF50", // Verde sutil, caso necessário para status
-    warning: "#FFC107", // Amarelo para alertas
-    error: "#F44336", // Vermelho para mensagens de erro
-    shadow: "rgba(0, 0, 0, 0.2)", // Sombra leve
+    // Cores principais
+
+    /** 
+     * Cor primaria
+     */
+    primary: "#0D0B26",
+    /** 
+     * Roxo claro, para botões de ação e destaques
+     */
+    secondary: "#7843EF",
+    /** 
+     * cor do background do app
+     */
+    background: "#F5F8FC",
+    /** 
+     * cor do background do botão de remover article do app
+     */
+    backgroundRemove: "#3C1B38",
+    /** 
+     * cor do background dos botões do app
+     */
+    backgroundButton: "#01071A",
+    /** 
+     * Branco, para áreas de texto ou cartões
+     */
+    surface: "#FFFFFF",
+    /** 
+     * Cor das bordas dos itens do app
+     */
+    border: "#EAE9EF",
+
+
+    // Texto
+    /** 
+     * Cor do texto principal 
+     */
+    text: "#010101",
+    /** 
+     * Cor do texto para constraste
+     */
+    textContrast: "#fff",
+    /** 
+     * Cor usada no subtitulo
+     */
+    textSub: "#878A92",
+    /** 
+     * Rosa vibrante, usado em links, interações ou texto de destaque
+     */
+    accent: "#FB5D81",
+
+    // Status
+    success: "#28A745",
+    /** 
+     * Verde, para mensagens de sucesso
+     */
+    warning: "#FFC107",
+    /**  
+     * Amarelo, para alertas
+     */
+    error: "#E84747",
+    /**  
+     * Vermelho, para mensagens de erro
+     */
+
+    // Elementos visuais
+    shadow: "rgba(0, 0, 0, 0.2)",
+    /**  
+     * Sombra leve para dar profundidade
+     */
 };
+
 
 export default colors;

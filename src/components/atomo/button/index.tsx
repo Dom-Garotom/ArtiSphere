31e1f8b32a6e4d61e237230163ac8939598eb801
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-12-02 10:30:07 -0300

    feat: criando o button default

---
+++ src/components/atomo/button/index.tsx
@@ -0,0 +1,38 @@
+import colors from "@/src/styles/color";
+import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+type Props = TouchableOpacityProps & {
+    color?: string,
+    text: string
+}
+
+export default function ButtonDefault({ color = colors.backgroundButton, text, ...props }: Props) {
+    return (
+        <TouchableOpacity 
+            style={[styles.button, { backgroundColor: color }]} 
+            activeOpacity={0.7}  {...props}
+        >
+            <Text style={styles.textButton}>
+                {text}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        flex: 1,
+        paddingHorizontal: 16,
+        paddingVertical: 4 ,
+        maxHeight: 56,
+        borderRadius: 12,
+        alignItems: "center",
+        justifyContent: 'center',
+    },
+
+    textButton: {
+        color: "white",
+        fontWeight: '500',
+        fontSize: 16,
+    },
+})
